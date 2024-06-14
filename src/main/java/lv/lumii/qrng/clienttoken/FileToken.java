f7dca6e8ddd10f6f2539e7ce7058c7986fdcf54f
Sergejs Kozlovics <sergejs.kozlovics@lumii.lv>
2024-06-15 01:34:10 +0300

    added TrustStore and expanded FileToken to support .pem files

--- src/main/java/lv/lumii/qrng/clienttoken/FileToken.java
+++ src/main/java/lv/lumii/qrng/clienttoken/FileToken.java
@@ -1,14 +1,20 @@
 package lv.lumii.qrng.clienttoken;
 
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.cactoos.scalar.Sticky;
 import org.cactoos.scalar.Unchecked;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.security.Key;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
+import java.io.FileReader;
+import java.security.*;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 
 public class FileToken implements Token {
 
@@ -16,10 +22,16 @@ public class FileToken implements Token {
     private final String alias;
     private final Unchecked<KeyStore> keyStore;
 
-    public FileToken(String fileName, String password, String alias) {
+    public FileToken(String pfxFileName, String password, String alias) {
         this.password = password.toCharArray();
         this.alias = alias;
-        this.keyStore = new Unchecked<>(new Sticky<>(() -> loadKeyStore(fileName)));
+        this.keyStore = new Unchecked<>(new Sticky<>(() -> loadKeyStore(pfxFileName)));
+    }
+
+    public FileToken(String[] pemCertFileNames, String pemKeyFileName, String keyPassword) {
+        this.password = keyPassword.toCharArray();
+        this.alias = "client/server";
+        this.keyStore = new Unchecked<>(new Sticky<>(() -> createKeyStoreFromPemFiles(pemCertFileNames, pemKeyFileName, keyPassword, this.alias)));
     }
 
     private KeyStore loadKeyStore(String fileName) throws Exception {
@@ -34,6 +46,37 @@ public class FileToken implements Token {
             instream.close();
         }
         return clientKeyStore;
+    }
+
+    private KeyStore createKeyStoreFromPemFiles(String[] pemCertFileNames, String pemKeyFileName, String keyPassword, String desiredAlias) throws Exception {
+        // Load the private key
+        PEMParser pemParser = new PEMParser(new FileReader(pemKeyFileName));
+        PrivateKeyInfo privateKeyInfo = (PrivateKeyInfo) pemParser.readObject();
+        // TODO: use keyPassword
+        if (!keyPassword.isEmpty())
+            throw new Exception("Password-encrypted PEM key files are not yet supported.");
+
+        JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+        PrivateKey privateKey = converter.getPrivateKey(privateKeyInfo);
+        pemParser.close();
+
+        // Load the certificates
+        ArrayList<X509Certificate> certs = new ArrayList<>();
+
+        for (String certFileName : pemCertFileNames) {
+            FileInputStream certReader = new FileInputStream(certFileName);
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(certReader);
+            certReader.close();
+            certs.add(certificate);
+        }
+
+        // Create the KeyStore
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        keyStore.load(null, null);
+        keyStore.setKeyEntry(desiredAlias, privateKey, keyPassword.toCharArray(), certs.toArray(new java.security.cert.Certificate[]{}));
+
+        return keyStore;
     }
 
     @Override
--- src/main/java/lv/lumii/qrng/clienttoken/TrustStore.java
+++ src/main/java/lv/lumii/qrng/clienttoken/TrustStore.java
@@ -1,2 +1,57 @@
-package lv.lumii.qrng.clienttoken;public class TrustStore {
+package lv.lumii.qrng.clienttoken;
+
+import org.cactoos.scalar.Unchecked;
+
+import javax.net.ssl.TrustManagerFactory;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+public class TrustStore {
+
+    private final Unchecked<KeyStore> trustStore;
+    public TrustStore(String pfxFileName, String password) {
+        this.trustStore = new Unchecked<>(()->loadTrustStore(pfxFileName, password));
+    }
+
+    public TrustStore(String[] pemCertFileNames) {
+        this.trustStore = new Unchecked<>(()->createTrustStore(pemCertFileNames));
+    }
+
+    private KeyStore loadTrustStore(String pfxFileName, String password) throws Exception {
+        KeyStore trustStore = KeyStore.getInstance(new File(pfxFileName), password.toCharArray());
+        return trustStore;
+    }
+
+    private KeyStore createTrustStore(String[] pemCertFileNames) throws Exception {
+
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        trustStore.load(null, null);
+
+        // Load the certificates from PEM file into the trustStore...
+        for (String certFileName : pemCertFileNames) {
+
+            FileInputStream certReader = new FileInputStream(certFileName);
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(certReader);
+            certReader.close();
+            trustStore.setCertificateEntry(certFileName, certificate);
+        }
+
+        return trustStore;
+    }
+
+    public TrustManagerFactory trustManagerFactory() {
+        // Create a TrustManagerFactory using the KeyStore
+        try {
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            //tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(this.trustStore.value());
+            return tmf;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
